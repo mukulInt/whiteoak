f588e7516a2bb7659abc6135b14c73c4b9f1e2ab
Mukul Kumar <mukul.kumar@indusnet.co.in>
2023-07-21 14:10:11 +0530

    test

--- src/main/java/com/whiteoakindia/whiteoak/WhiteoakApplication.java
+++ src/main/java/com/whiteoakindia/whiteoak/WhiteoakApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:constant/constant.properties")
 public class WhiteoakApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhiteoakApplication.class, args);
